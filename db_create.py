f1840879cb7eaf77f6a89c218ea730871c312f35
Hobinx <binservmail@gmail.com>
2017-11-08 14:42:20 +0800

    fix sql syntax error

--- db_create.py
+++ db_create.py
@@ -17,11 +17,11 @@ with sqlite3.connect(DATABASE_PATH) as connection:
     """)
 
     c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status'
+        'INSERT INTO tasks (name, due_date, priority, status)'
         'VALUES("Finish this tutorial", "03/25/2017", 10, 1)'
     )
 
     c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status'
+        'INSERT INTO tasks (name, due_date, priority, status)'
         'VALUES("Finish Real Python Course 2", "03/25/2017", 10, 1)'
     )
