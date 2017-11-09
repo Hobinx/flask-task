c68b63b67c2f388bab20a73ab6ca7f22624f8e57
Hobinx <binservmail@gmail.com>
2017-11-09 08:40:46 +0800

    update config and populate db with SQLAlchemy

--- db_create.py
+++ db_create.py
@@ -1,27 +1,10 @@
-import sqlite3
+from views import db
+from models import Task
+from datetime import date
 
-from _config import DATABASE_PATH
+db.create_all()
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+db.session.add(Task("Finish this tutorial", date(2017, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2017, 10, 3), 10, 1))
 
-    c = connection.cursor()
-
-    c.execute("""
-        CREATE TABLE tasks
-          (task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-           name TEXT NOT NULL,
-           due_date TEXT NOT NULL,
-           priority INTEGER NOT NULL,
-           status INTEGER NOT NULL
-          )
-    """)
-
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "03/25/2017", 10, 1)'
-    )
-
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2017", 10, 1)'
-    )
+db.session.commit()
